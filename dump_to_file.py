f9365f1214730d7d67a87dd2b803b8aa99b8e5f2
Kaarel Moppel <kaarel.moppel@gmail.com>
2024-11-29 14:25:26 +0200

    Cache Vte.get_minor_version as static + don't write empty files

--- dump_to_file.py
+++ dump_to_file.py
@@ -22,6 +22,7 @@ AVAILABLE = ['DumpToFile']
 class DumpToFile(plugin.MenuItem):
     capabilities = ['terminal_menu']
     dumpers = None
+    vte_minor_version = None
 
     def __init__(self):
         plugin.MenuItem.__init__(self)
@@ -35,6 +36,8 @@ class DumpToFile(plugin.MenuItem):
             item = Gtk.MenuItem.new_with_mnemonic(_('D_ump terminal to file'))
             item.connect("activate", self.dump_console, terminal)
             menuitems.append(item)
+            self.vte_minor_version = Vte.get_minor_version()
+            dbg("Vte.get_minor_version(): %s" % self.vte_minor_version)
 
     def dump_console(self, _widget, Terminal):
         """ Handle menu item callback by saving console text to a predefined location and creating the ~/.terminator folder if necessary """
@@ -45,13 +48,12 @@ class DumpToFile(plugin.MenuItem):
             log_file = "console_" + datetime.datetime.now().strftime('%Y-%m-%d_%H%M%S') + ".log"
             vte = Terminal.get_vte()
             dbg("Terminal.get_vte(): %s" % vte)
-            dbg("Vte.get_minor_version(): %s" % Vte.get_minor_version())
             col, row = vte.get_cursor_position()
-            if Vte.get_minor_version() < 72:
+            if self.vte_minor_version and self.vte_minor_version < 72:
                 content = vte.get_text_range(0, 0, row, col, lambda *a: True)
             else:
                 content = vte.get_text_range_format(Vte.Format.TEXT, 0, 0, row, col)
-            if content:
+            if content and content[0]:
                 fd = open(os.path.join(log_folder, log_file), 'w+')
                 fd.write(content[0])
                 fd.flush()
